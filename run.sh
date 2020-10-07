953a2c47ac1f5eb4d7c31c69ad487e87b8f79ae0
Vadim Grinco <vadim@grinco.eu>
2020-10-07 22:51:44 +0100

    Added a simple script that would start/stop the docker container for integration with home assistant
    Signed-off-by: Vadim Grinco <vadim@grinco.eu>

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+LAST='null'
+while [ true ]; do
+  SYNC=`pigs r 14`
+  if [ $LAST == $SYNC ]; then
+     sleep 5
+  elif [ $SYNC -eq 0 ]; then
+     echo "Stopping HUE Sync"
+     docker stop harmonizeproject
+  else
+     echo "Starting HUE Sync"
+     docker run -d --rm -t --name harmonizeproject --device=/dev/video0 -e ARGS="-v -g4" -v ~/client.json:/app/client.json grinco/harmonizeproject:armhf 
+  fi
+  LAST=$SYNC
+done
